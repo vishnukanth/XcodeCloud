b3cfe07d7e147b5d57bcfefb747bc7708aa740a3
vishnukanth <vishnukanth.alaparthi@ncs.com.sg>
2022-11-11 09:32:20 +0800

    intial commit

---
+++ TestXcodeCloud/TestXcodeCloud/TestXcodeCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestXcodeCloudApp.swift
+//  TestXcodeCloud
+//
+//  Created by Vishnukanth Alaparthi (NCS) on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TestXcodeCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
